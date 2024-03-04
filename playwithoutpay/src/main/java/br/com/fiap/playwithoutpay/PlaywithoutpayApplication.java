52c2adba29925d21c09c39d837a4e9cb8d8c6446
Santlago <breno.santiago.8@gmail.com>
2024-03-04 13:21:22 -0300

    feat: creating project

---
+++ playwithoutpay/src/main/java/br/com/fiap/playwithoutpay/PlaywithoutpayApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.playwithoutpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaywithoutpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaywithoutpayApplication.class, args);
+	}
+
+}
---
+++ playwithoutpay/src/test/java/br/com/fiap/playwithoutpay/PlaywithoutpayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.playwithoutpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaywithoutpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
