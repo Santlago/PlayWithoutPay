7e9a902868fbbc685bc4f34469100dba286e61c2
Santlago <breno.santiago.8@gmail.com>
2024-04-25 12:03:08 -0300

    fix: edit

--- web-playwithoutpay/src/app/comments/[id]/page.tsx
+++ web-playwithoutpay/src/app/comments/[id]/page.tsx
@@ -1,4 +1,3 @@
-"use client"
 
 import { getById } from "@/app/actions/comments/get-by-id";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
@@ -8,13 +7,13 @@ import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 export default async function EditComment({params}: Params) {
     const { id } = params
     console.log(id)
-    // const comment: Comment = await getById(id)
-    // console.log(comment)
+    const comment: Comment = await getById(id)
+    console.log(comment)
 
     return (
         <div>
             <h1>{id}</h1>
-            {/* <h2>{comment.title}</h2> */}
+            <h2>{comment.title}</h2>
         </div>
     )
 }
